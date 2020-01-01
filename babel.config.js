f6186efc976ee5fff3b547ca19be2304c9c8a4ef
gauravcoditas <gaurav.mohapatra@coditas.com>
2020-01-01 10:16:28 +0530

    Added tsconfig,  tslint , added relative path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.ts', '.android.ts', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          'App': './src/App/',
+          'Components': './src/App/Components',
+          'Actions': './src/App/Actions',
+          'Assets': './src/App/Assets',
+          'Config': './src/App/Config',
+          'Cointainers': './src/App/Cointainers',
+          'Navigators': './src/App/Navigators',
+          'Reducers': './src/App/Reducers',
+          'Services': './src/App/Services',
+          'Stores': './src/App/Stores',
+          'Theme': './src/App/Theme',
+        },
+      },
+    ],
+  ],
 };
