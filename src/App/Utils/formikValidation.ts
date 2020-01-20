4e39fb9049d321f80c027dea4e27ee25829615e0
gauravcoditas <gaurav.mohapatra@coditas.com>
2020-01-20 18:52:20 +0530

    Added login form validation

---
+++ src/App/Utils/formikValidation.ts
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+const validEmail = 'Please enter valid email address';
+const emailReq = 'Email address is required';
+const passwordRequired = 'Password is required';
+const comparePassword = 'Password length must be greater than 8 and less than 20';
+
+// const confirmPasswordRequired = 'Confirm password is required';
+
+// const passwordMatch = 'Password must match';
+
+export const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email(validEmail)
+    .required(emailReq),
+  password: Yup.string()
+    .required(passwordRequired)
+    .matches(/^[a-zA-Z0-9\W]{8,20}$/, comparePassword),
+});
