18c6d3b4a8dc462c19e54f0fdc65908aeedfdfb9
gauravcoditas <gaurav.mohapatra@coditas.com>
2020-01-21 19:23:23 +0530

    added sign up , default header

--- src/App/Navigators/AppNavigator.ts
+++ src/App/Navigators/AppNavigator.ts
@@ -3,6 +3,8 @@ import { createStackNavigator } from 'react-navigation-stack';
 import ExampleScreen from 'Containers/Example/ExampleScreen';
 import SplashScreen from 'Containers/SplashScreen/SplashScreen';
 import LoginScreen from 'App/Containers/Login/LoginScreen';
+import SignupScreen from 'App/Containers/Signup/SignupScreen';
+import { Colors } from 'Theme';
 
 /**
  * The root screen contains the application's navigation.
@@ -11,11 +13,23 @@ const StackNavigator = createStackNavigator(
   {
     SplashScreen: SplashScreen,
     LoginScreen: LoginScreen,
+    Signupscreen: SignupScreen,
     MainScreen: ExampleScreen,
   },
   {
     initialRouteName: 'SplashScreen',
-    headerMode: 'none',
+    headerMode: 'screen',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Colors.primary,
+        borderBottomWidth: 0,
+        elevation: 0,
+      },
+      headerTintColor: Colors.background,
+      headerBackTitleStyle: {
+        color: Colors.background,
+      },
+    },
   },
 );
 
